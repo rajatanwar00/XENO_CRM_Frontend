b489ff0ef9d9947dffc86d4da4c13655784c1f25
rajatanwar00 <apj.rajat@gmail.com>
2024-11-18 10:02:40 +0530

    Loader added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,8 @@
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
+import Loader from '../Loader/Loader';
 
 const clientID=import.meta.env.VITE_CLIENTID;
 const backendURL=import.meta.env.VITE_APIURL;
@@ -11,20 +12,24 @@ console.log(clientID,backendURL);
 function Login() {
 
     const navigate=useNavigate()
+    const [loading, setLoading] = useState(false);
 
     function handleLoginSuccess(response){
         console.log('Login Successful');
 
+        setLoading(true);
         axios.post(`${backendURL}/login`,{
             authToken:response.credential
         })
         .then((response)=>{
             //console.log(response)
             localStorage.setItem('jwtToken',response.data.token)
+            setLoading(false)
             navigate('/audience')
         })
         .catch((err)=>{
             console.log(err);
+            setLoading(false)
         })
 
     }
@@ -35,12 +40,15 @@ function Login() {
 
   return (
     <div className='h-screen flex justify-center items-center'>
-        <div className='border p-5 shadow-xl bg-slate-100 rounded-sm '>
-            <p>Login to continue</p><br/>
-            <GoogleOAuthProvider clientId={clientID}>
-                <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError}/>
-            </GoogleOAuthProvider>
-        </div>        
+        {loading?(<Loader className='items-center'/>):(
+            <div className='border p-5 shadow-xl bg-slate-100 rounded-sm '>
+                <p>Login to continue</p><br/>
+                <GoogleOAuthProvider clientId={clientID}>
+                    <GoogleLogin onSuccess={handleLoginSuccess} onError={handleLoginError}/>
+                </GoogleOAuthProvider>
+            </div>   
+        )}
+             
     </div>
   )
 }
